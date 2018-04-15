c2fb80117f1ad1fe4ab9f2af6ce5e7b062026e94
pmlaforest <pmlaforest@hotmail.com>
2018-04-15 14:57:47 -0400

    cleanup of search function

--- virtual_library/search/views.py
+++ virtual_library/search/views.py
@@ -42,32 +42,7 @@ def search(request):
                         authors.append(a1.name)
                     books_and_authors.append((b, authors))
 
-
         nb_books = len(books)
-
-        '''
-        books_and_authors = [] 
-        
-        #The keyword is an author
-        books_found_when_keyword_is_an_author = Book.objects.filter(authors__name=keyword)
-
-        if books_found_when_keyword_is_an_author:
-            for b in books_found_when_keyword_is_an_author:
-                books_and_authors.append((b.title, keyword))
-
-        #The keyword is a book
-        books_found_when_keyword_is_a_book = Book.objects.filter(title=keyword)
-        
-        if books_found_when_keyword_is_a_book:
-            for b2 in books_found_when_keyword_is_a_book:
-                author_of_book = Author.objects.filter(book__title=b2)
-                for author in author_of_book:         
-                    books_and_authors.append((b2, author.name))
-
-   
-        nb_books = len(books_and_authors)
-             '''
-
 
         context = { 'nb_books': nb_books,
                     'books_and_authors': books_and_authors,
