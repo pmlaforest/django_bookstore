410d88c7485e921e2127d4a3905ad021633fb940
pmlaforest <pmlaforest@hotmail.com>
2018-04-15 14:20:50 -0400

    finished search functionnality

--- virtual_library/search/views.py
+++ virtual_library/search/views.py
@@ -16,9 +16,38 @@ def search(request):
     except (KeyError):
         context = { 'keyword': ""}
     else:
+        books = []
+        authors = []
+        books_and_authors = [] 
+
+        #The keyword is an author
+        books_found_when_keyword_is_an_author = Book.objects.filter(authors__name=keyword)
+        if books_found_when_keyword_is_an_author:
+            for b1 in books_found_when_keyword_is_an_author:
+                books.append(b1.title)
+
+        #The keyword is a book
+        books_found_when_keyword_is_a_book = Book.objects.filter(title=keyword)
+        if books_found_when_keyword_is_a_book:
+            for b2 in books_found_when_keyword_is_a_book:
+                books.append(b2.title)
+        
+        #Getting the authors of the books
+        if books:
+            for b in books:
+                authors = []
+                authors_of_book_queryset = Author.objects.filter(book__title=b)
+                if authors_of_book_queryset:
+                    for a1 in authors_of_book_queryset:                     
+                        authors.append(a1.name)
+                    books_and_authors.append((b, authors))
+
+
+        nb_books = len(books)
+
+        '''
         books_and_authors = [] 
         
-
         #The keyword is an author
         books_found_when_keyword_is_an_author = Book.objects.filter(authors__name=keyword)
 
@@ -35,7 +64,10 @@ def search(request):
                 for author in author_of_book:         
                     books_and_authors.append((b2, author.name))
 
+   
         nb_books = len(books_and_authors)
+             '''
+
 
         context = { 'nb_books': nb_books,
                     'books_and_authors': books_and_authors,
