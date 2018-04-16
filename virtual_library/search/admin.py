a4f13bc1697790fda403bc0837a11ba449b0ce7b
tynail <marc4283@hotmail.com>
2018-04-16 12:41:44 -0400

    search delete

--- virtual_library/search/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Book, Author, Book_Author
-
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Book_Author)
--- virtual_library/search/tests.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-# CECI EST UN TEST ------------------------------------------------
